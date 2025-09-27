d531af9f1b3f85bcbe906d21fcbde7203a4fecad
StephenRay2 <smray1@byu.edu>
2025-09-26 21:16:56 -0700

    Added player images and animations

--- main.py
+++ main.py
@@ -12,12 +12,38 @@ dt = 0
 size = 64
 shift_multiplier = 1
 dungeon_depth = 0
-font = pygame.font.SysFont(None, 48)
+font = pygame.font.SysFont(None, 24)
 scroll = 0
 player_speed = 300
 dungeon_traversal_speed = 50
 
-# ########## BACKGROUND IMAGES ####################
+
+############ PLAYER IMAGES #################
+
+player_stand_image = pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CharacterCorynnFrontStanding.png")
+player_stand_image_back = pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CharacterCorynnBackStanding.png")
+player_stand_left = pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CharacterCorynnLeftStanding.png")
+player_stand_right = pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CharacterCorynnRightStanding.png")
+player_walk_down_images = [pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkDown1.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkDown2.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkDown3.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkDown4.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkDown5.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkDown6.png").convert_alpha()]
+player_walk_up_images = [pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkUp1.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkUp2.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkUp3.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkUp4.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkUp5.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkUp6.png").convert_alpha()]
+player_walk_left_images = [pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkLeft1.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkLeft2.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkLeft3.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkLeft4.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkLeft5.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkLeft6.png").convert_alpha(), pygame.image.load("/Users/stephenray/CodeProjects/Catrun/assets/sprites/player/CorynnWalkLeft7.png").convert_alpha()]
+
+
+player_stand_image = pygame.transform.scale(player_stand_image, (size, size))
+player_stand_image_back = pygame.transform.scale(player_stand_image_back, (size, size))
+player_stand_left = pygame.transform.scale(player_stand_left, (size, size))
+player_stand_right = pygame.transform.scale(player_stand_right, (size, size))
+player_walk_down_images = [pygame.transform.scale(img, (size, size)) for img in player_walk_down_images]
+player_walk_up_images = [pygame.transform.scale(img, (size, size)) for img in player_walk_up_images]
+player_walk_left_images = [pygame.transform.scale(img, (size, size)) for img in player_walk_left_images]
+player_walk_right_images = [pygame.transform.flip(img, True, False) for img in player_walk_left_images]
+
+player_frame_index = 0
+player_animation_timer = 0
+player_current_image = player_stand_image
+last_direction = "down"
+
+############ BACKGROUND IMAGES ####################
 
 
 bg_green = pygame.Surface((width, height))
@@ -125,13 +151,66 @@ while running:
                          y * bg_height + scroll_y % bg_height))
 
 
-    pygame.draw.rect(screen, "red", (player_pos.x - size/2, player_pos.y - size/2, size, size))
+    screen.blit(player_current_image, (player_pos.x - size/2, player_pos.y - size/2))
+
 
     keys = pygame.key.get_pressed()
+    if not (keys[pygame.K_w] or keys[pygame.K_s] or keys[pygame.K_a] or keys[pygame.K_d]):
+        if last_direction == "down":
+            player_current_image = player_stand_image
+        elif last_direction == "up":
+            player_current_image = player_stand_image_back
+        elif last_direction == "left":
+            player_current_image = player_stand_left
+        elif last_direction == "right":
+            player_current_image = player_stand_right
+
+
     if keys[pygame.K_w]:
+        last_direction = "up"
+        player_animation_timer += dt
+        if keys[pygame.K_LSHIFT]:
+            if player_animation_timer > .04:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_up_images)
+                player_current_image = player_walk_up_images[player_frame_index]
+                player_animation_timer = 0
+        else:
+            if player_animation_timer > .07:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_up_images)
+                player_current_image = player_walk_up_images[player_frame_index]
+                player_animation_timer = 0
+    if keys[pygame.K_w] and (player_pos.y - (size/2)) >= 0:
         player_pos.y -= player_speed * dt * shift_multiplier
+    
     if keys[pygame.K_s]:
+        last_direction = "down"
+        player_animation_timer += dt
+        if keys[pygame.K_LSHIFT]:
+            if player_animation_timer > .04:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_down_images)
+                player_current_image = player_walk_down_images[player_frame_index]
+                player_animation_timer = 0
+        else:
+            if player_animation_timer > .07:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_down_images)
+                player_current_image = player_walk_down_images[player_frame_index]
+                player_animation_timer = 0
+    if keys[pygame.K_s] and (player_pos.y + (size/2)) <= height:
         player_pos.y += player_speed * dt * shift_multiplier
+
+    if keys[pygame.K_a]:
+        last_direction = "left"
+        player_animation_timer += dt
+        if keys[pygame.K_LSHIFT]:
+            if player_animation_timer > .04:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_left_images)
+                player_current_image = player_walk_left_images[player_frame_index]
+                player_animation_timer = 0
+        else:
+            if player_animation_timer > .07:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_left_images)
+                player_current_image = player_walk_left_images[player_frame_index]
+                player_animation_timer = 0
     if keys[pygame.K_a] and 0 < dungeon_depth < 50000:
         scroll_x += player_speed * dt * shift_multiplier
         dungeon_depth -= dungeon_traversal_speed * shift_multiplier
@@ -141,6 +220,20 @@ while running:
     elif keys[pygame.K_a] and dungeon_depth >= 50000:
         player_pos.x -= player_speed * dt * shift_multiplier
         dungeon_depth -= dungeon_traversal_speed * shift_multiplier
+
+    if keys[pygame.K_d]:
+        last_direction = "right"
+        player_animation_timer += dt
+        if keys[pygame.K_LSHIFT]:
+            if player_animation_timer > .04:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_right_images)
+                player_current_image = player_walk_right_images[player_frame_index]
+                player_animation_timer = 0
+        else:
+            if player_animation_timer > .07:
+                player_frame_index = (player_frame_index + 1) % len(player_walk_right_images)
+                player_current_image = player_walk_right_images[player_frame_index]
+                player_animation_timer = 0
     if keys[pygame.K_d] and 0 < dungeon_depth < 50000:
         scroll_x -= player_speed * dt * shift_multiplier
         dungeon_depth += dungeon_traversal_speed * shift_multiplier
@@ -150,6 +243,7 @@ while running:
     elif keys[pygame.K_d] and dungeon_depth >= 50000:
         player_pos.x += player_speed * dt * shift_multiplier
         dungeon_depth += dungeon_traversal_speed * shift_multiplier
+
     if keys[pygame.K_ESCAPE]:
         running = False
     if keys[pygame.K_LSHIFT or pygame.K_RSHIFT]:
@@ -157,9 +251,13 @@ while running:
     else:
         shift_multiplier = 1
 
-
+    
     depth_text = font.render(f"Dungeon Depth: {dungeon_depth:.1f}", True, (255, 255, 255))
-    screen.blit(depth_text, (20,20))
+    depth_rect = pygame.Rect(20, 20, depth_text.get_width(), depth_text.get_height())
+    temp_surface = pygame.Surface((depth_rect.width + 10, depth_rect.height + 10), pygame.SRCALPHA)
+    temp_surface.fill((0, 0, 0, 100))
+    screen.blit(temp_surface, (depth_rect.x, depth_rect.y))
+    screen.blit(depth_text, (depth_rect.x + 5, depth_rect.y + 5))
 
 
     pygame.display.flip()
