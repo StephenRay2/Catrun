cbd1d0b9ef04359fbbddfc63ada46fd098f39a40
StephenRay2 <smray1@byu.edu>
2025-11-13 15:10:29 -0700

    added a new debug feature to print mob names

--- printpicturenames.py
+++ printpicturenames.py
@@ -1,9 +1,21 @@
 import os
 
-folder_path = "assets/sprites/items"
-i = 0
-for picture in os.listdir(folder_path):
-    i += 1
-    if picture.endswith((".png", ".jpg", ".jpeg")):
-        print(f"{i}. {picture}")
+def print_items_list():
+    folder_path = "assets/sprites/items"
+    i = 0
+    for picture in os.listdir(folder_path):
+        i += 1
+        if picture.endswith((".png", ".jpg", ".jpeg")):
+            print(f"{i}. {picture}")
 
+
+def print_creature_list():
+    folder_path = "assets/sprites/mobs"
+    i = 0
+    for picture in os.listdir(folder_path):
+        i += 1
+        if picture.endswith((".png", ".jpg", ".jpeg")):
+            print(f"{i}. {picture}")
+
+
+print_creature_list()
